8ab3a52453d6af8842f4e3e6740c5d2706653795
babma04 <joaocarrilholouro@gmail.com>
2026-08-22 18:52:28 +0100

    Initializes the main Command pattern matcher. Creates a thread and calls start on the watcher.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
-use file_syncer::cli::parse_args;
-use file_syncer::types::SyncerError;
+use std::{thread, time::Duration, sync::mpsc};
+
+use clap::Command;
+
+use file_syncer::{cli::parse_args, types::{Command, SyncError}, watcher::start_watcher};
 
 fn main () -> Result<(), SyncerError> {
-    let _config = parse_args()?;
+    let config = parse_args()?;
+    // Channel to communicate with the watcher thread
+    let (tx, rx) = mpsc::channel();
+    match config {
+        Command::Watch {source, destination, verbose, debounce} => {
+            // watcher runs on a different thread
+            let handle = thread::spawn(move || {
+                todo!("Call start watcher. Pattern match watcher result and call a sync action.");
+                let notice = start_watcher(source, tx);
 
+                // Sleeps through a debouncing period to avoid race conditions or deadlocks
+                thread::sleep(Duration::from_millis(debounce));
+            });
+
+            handle.join().unwrap();
+        }
+
+        // No other command for now.
+        // Match other commands as added
+    }
 
     Ok(())
 }
